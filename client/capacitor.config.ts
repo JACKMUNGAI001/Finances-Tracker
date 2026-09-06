321a64c480857c8755875ee82a9b0fd383f5c2ea
Jackson <jacksonmungai001@gmail.com>
2026-09-06 20:15:33 +0300

    new update

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const devServerUrl = process.env.DEV_SERVER_URL
-const vercelUrl = 'https://client-delta-fawn-82.vercel.app'
+const vercelUrl = 'https://finances-tracker-gilt.vercel.app'
 
 const config: CapacitorConfig = {
   appId: 'com.financetracker.app',
